f8839f8082f9417bd088feccca76ee48709944f6
Mark Vodyanitskiy <mvodya@gmail.com>
2017-01-27 23:03:15 +1000

    Функции шифрования

--- COClib/COC.cs
+++ COClib/COC.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,26 @@ namespace COClib
 {
     public class COC
     {
+        public static Aes masterKey;
+
+
+        // Генератор ключа
+        public static void GenKey()
+        {
+            masterKey = Aes.Create();
+        }
+
+        // Шифровка
+        public static String Encrypt()
+        {
+            byte[] encrypted = CryptMaster.EncryptStringToBytesAes(original, myAes.Key, myAes.IV);
+            return "";
+        }
+
+        // Расшифровка
+        public static String Decrypt()
+        {
+            return "";
+        }
     }
 }
---
+++ COClib/CryptMaster.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace COClib
+{
+    class CryptMaster
+    {
+        public static byte[] EncryptStringToBytesAes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Проверка аргументов
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Создаем объект класса AES
+            // с определенным ключом and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для шифрования.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Записываем в поток все данные.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            //Возвращаем зашифрованные байты из потока памяти.
+            return encrypted;
+
+        }
+
+        public static string DecryptStringFromBytesAes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Проверяем аргументы
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Строка, для хранения расшифрованного текста
+            string plaintext;
+
+            // Создаем объект класса AES,
+            // Ключ и IV
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для расшифрования.
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Читаем расшифрованное сообщение и записываем в строку
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+    }
+}
