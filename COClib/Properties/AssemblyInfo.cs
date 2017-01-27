eb23c9925eacd48d4c5d5a535b549325f571d0fc
Mark Vodyanitskiy <mvodya@gmail.com>
2017-01-27 22:13:32 +1000

    Поправка библеотеки

--- COClib/Properties/AssemblyInfo.cs
+++ COClib/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Общие сведения об этой сборке предоставляются следующим набором
-// набором атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные с этой сборкой.
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
 [assembly: AssemblyTitle("COClib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("COClib")]
-[assembly: AssemblyCopyright("©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("ru")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c23dbbe5-bc52-4a87-a493-8cd8351b9a68")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
@@ -24,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Редакция
 //
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию
-// указав "*", как показано ниже:
+// используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
