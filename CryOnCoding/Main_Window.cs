b106dfb2498ec397ac2756b5889fc36baebab5a8
Ярославцев Владислав <Painter_ProudLion@mail.ru>
2017-01-27 21:34:13 +1000

    Добавил главное окно

--- CryOnCoding/Main_Window.cs
+++ CryOnCoding/Main_Window.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CryOnCoding
 {
-    public partial class Form1 : Form
+    public partial class Main_Window : Form
     {
-        public Form1()
+        public Main_Window()
         {
             InitializeComponent();
         }
